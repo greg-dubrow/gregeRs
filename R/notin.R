fcd604398ec8b6c5ad4fb6d9fd67790488df50b4
greg-dubrow <dubrowg@gmail.com>
2025-05-29 09:51:44 +0200

    notin documentation

--- R/notin.R
+++ R/notin.R
@@ -1,4 +1,12 @@
+#' Finds observations not in a vector, using the negate function for %in%
+#'
+#' @param what Creates a %notin% operator select observations not in a defined vector
+#' @usage %notin% c(" ", " "))
+#' @return A new column or filtered data-frame based on the %notin% selection
+#' @export
+#'
+#' @examples
+#' filter(state %notin% c("PA", "MD))
 
 
-# create notin operator to help with cleaning & analysis
 `%notin%` <- purrr::negate(`%in%`)
